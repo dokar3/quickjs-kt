ed823b7bc42bb396292d495066fa9c4dcaeec149
dokar3 <68095777+dokar3@users.noreply.github.com>
2024-02-12 23:24:39 +0800

    Fix extract options

--- quickjs/scripts/setupMultiplatformJdks.ts
+++ quickjs/scripts/setupMultiplatformJdks.ts
@@ -106,9 +106,9 @@ async function downloadAndExtractJdk(jdk: Jdk) {
 
   await $`mkdir ${extractPath}`;
   if (filename.endsWith(".gz")) {
-    throwIfStderrNotEmpty(await $`tar -xzf ${filepath} -C ${extractPath} --quiet`);
+    throwIfStderrNotEmpty(await $`tar -xzf ${filepath} -C ${extractPath}`);
   } else if (filename.endsWith(".zip")) {
-    throwIfStderrNotEmpty(await $`unzip  -o --quiet ${filepath} -d ${extractPath}`);
+    throwIfStderrNotEmpty(await $`unzip -o --q ${filepath} -d ${extractPath}`);
   } else {
     throw new Error(`Unknown JDK archive: ${filename}`);
   }
