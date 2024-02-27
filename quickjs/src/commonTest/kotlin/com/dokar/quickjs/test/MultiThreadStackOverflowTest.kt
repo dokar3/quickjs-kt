2a6818b3ab406f0d14f57bcd290df52ba8ed83d9
dokar3 <68095777+dokar3@users.noreply.github.com>
2024-02-27 21:15:49 +0800

    Update test

--- quickjs/src/commonTest/kotlin/com/dokar/quickjs/test/MultiThreadStackOverflowTest.kt
+++ quickjs/src/commonTest/kotlin/com/dokar/quickjs/test/MultiThreadStackOverflowTest.kt
@@ -5,6 +5,7 @@ import com.dokar.quickjs.quickJs
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.IO
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
@@ -14,15 +15,17 @@ class MultiThreadStackOverflowTest {
     fun stackOverflowWhenExecutingJobs() = runTest {
         // This ensures that our async jobs are executed in different threads
         quickJs(jobDispatcher = Dispatchers.IO.limitedParallelism(100)) {
-            asyncFunction("runJob") {}
+            asyncFunction("runJob") { delay(50) }
 
             // This CAN throw a stack overflow error if executePendingJob()
             // is not thread-safe.
             evaluate<Any?>(
                 """
+                    const jobs = [];
                     for (let i = 0; i < 1000; i++) {
-                        await runJob();
+                        jobs.push(runJob());
                     }
+                    await Promise.all(jobs);
                 """.trimIndent()
             )
         }
