cba0519945cd801960c1b7e731f6b022f637cdfd
dokar3 <68095777+dokar3@users.noreply.github.com>
2026-02-08 19:54:31 +0800

    Initialize stack canary with random value

--- quickjs/native/common/stack_chk.c
+++ quickjs/native/common/stack_chk.c
@@ -1,10 +1,31 @@
 #include <stdlib.h>
 #include <stdint.h>
+#include <windows.h>
 
 #if defined(_WIN32)
+
+// Default stack guard value
+uintptr_t __stack_chk_guard = 0x595e9fbd94fda766;
+
 void __stack_chk_fail(void) {
     abort();
 }
 
-uintptr_t __stack_chk_guard = 0x595e9fbd94fda766;
+typedef BOOLEAN (WINAPI *RtlGenRandomFunc)(PVOID, ULONG);
+
+// Initialize the stack guard with a random value
+__attribute__((constructor))
+static void __stack_chk_init(void) {
+    HMODULE hAdvApi32 = LoadLibraryA("advapi32.dll");
+    if (hAdvApi32) {
+        RtlGenRandomFunc RtlGenRandom = (RtlGenRandomFunc)GetProcAddress(hAdvApi32, "SystemFunction036");
+        if (RtlGenRandom) {
+            uintptr_t random_guard;
+            if (RtlGenRandom(&random_guard, sizeof(random_guard))) {
+                __stack_chk_guard = random_guard;
+            }
+        }
+        FreeLibrary(hAdvApi32);
+    }
+}
 #endif
